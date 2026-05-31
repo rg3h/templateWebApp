b7e3e4029e5a753c347f1be2a048463980a2799a
rg3h <rich.gossweiler@gmail.com>
2026-05-31 18:29:21 -0400

    updated runSimpleServer.bat for readability

--- tools/runSimpleServer.bat
+++ tools/runSimpleServer.bat
@@ -4,9 +4,10 @@ clear
 setlocal
 color 1F
 
-:: %~d0 is the drive and %~p0 is the code's directory
 :: pushd works for UNC paths (\\server\...) cd does not
+:: %~d0 is the drive and %~p0 is the code's directory
 pushd %~d0%~p0simpleServer
+
 set NO_COLOR=1
 cmd /k node . --webroot ../../public
 
